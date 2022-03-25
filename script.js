1196f40472358cd5861d777983693e89dc422d27
Nathan Prater <nprater86@gmail.com>
2022-03-25 16:10:43 -0500

    finished assignment

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-var likes = 0;
-var numLikes = document.querySelector('#numLikes');
+var likes = [0,0,0];
 
-function increaseLikes() {
-    likes++;
-    numLikes.innerText = likes;
+function increaseLikes(element) {
+    if(element.id === 'neilmLikes') {
+        likes[0]++;
+        element.innerText = likes[0];
+    } else if(element.id === 'nicholekLikes'){
+        likes[1]++;
+        element.innerText = likes[1];
+    } else if(element.id === 'jimrLikes') {
+        likes[2]++;
+        element.innerText = likes[2];
+    }
 }
